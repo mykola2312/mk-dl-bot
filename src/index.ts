ad955a64513379fb846be684e86a0c5191b909bf
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-09-18 08:10:17 +0300

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { Bot, BotOptions } from "gramio"
+import type { BotOptions } from 'gramio'
+import { Bot } from 'gramio'
 
 const bot = new Bot({
-    token: Deno.env.get("BOT_TOKEN"),
-    api: {
-        baseURL: Deno.env.get("BOT_API_URL"),
-    },
-} as BotOptions);
+  token: Deno.env.get('BOT_TOKEN'),
+  api: {
+    baseURL: Deno.env.get('BOT_API_URL'),
+  },
+} as BotOptions)
 
-bot.command("test", (ctx) => {
-    ctx.send("yooo wassup");
-});
+bot.command('test', (ctx) => {
+  ctx.send('yooo wassup')
+})
 
-bot.start();
+bot.start()
